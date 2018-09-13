f30ea554b18be0ab6b093e5000336907956dc2f2
Ingemar <iingemar@gmail.com>
2018-09-13 15:01:07 +0200

    Added enzyme test for CommentBox

--- testing-with-react/src/components/__test__/App.test.js
+++ testing-with-react/src/components/__test__/App.test.js
@@ -1,18 +1,14 @@
 // Could also be called App.js but calling it App.test.js adds more clarity what it is.
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    // Enzymes shallow renders *just* the given component and none of its React children.
+    // Wrapped version of our app component. Has additional Enzyme functionality on top!
+    const wrapped = shallow(<App/>);
 
-    ReactDOM.render(<App/>, div);
 
-    // div.innerHTML
-    // <div><div>CommentBox</div><div>CommentList</div></div>
-    expect(div.innerHTML).toContain('CommentBox');
-
-    // Make sure to cleanup
-    ReactDOM.unmountComponentAtNode(div);
 });
