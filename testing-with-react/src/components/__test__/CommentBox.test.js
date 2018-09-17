0072e296c26e1c9db7c268c8544d9bc8db6dab91
Ingemar <iingemar@gmail.com>
2018-09-17 10:51:34 +0200

    CommentBox: added describe block for textarea tests

--- testing-with-react/src/components/__test__/CommentBox.test.js
+++ testing-with-react/src/components/__test__/CommentBox.test.js
@@ -25,31 +25,32 @@ it('has a textarea and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        // Return fake event object
-        target: { value: 'new comment' }
+// creates a block that groups together several related tests in one "test suite".
+describe('the text area', () => {
+
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            // Return fake event object
+            target: { value: 'new comment' }
+        });
+
+        // Force update component
+        wrapped.update();
     });
 
-    // Force update component
-    wrapped.update();
-
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('empties textarea on submit form', () => {
-    // Simulate user writing something in the textarea
-    wrapped.find('textarea').simulate('change', {
-        // Return fake event object
-        target: { value: 'new comment' }
+    it('has a textarea that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
     });
-    wrapped.update();
-    // Good to check that we actually changed the textarea, so we don't get a false positive on next expect.
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 
-    // Simulate user submitting the form
-    wrapped.find('form').simulate('submit');
-    wrapped.update();
+    it('empties textarea on submit form', () => {
+        // Good to check that we actually changed the textarea, so we don't get a false positive on next expect.
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
+        // Simulate user submitting the form
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+
 });
