51ec1374f70f431d7d4971e0f40814bafc21a541
Ingemar <iingemar@gmail.com>
2018-09-26 15:22:00 +0200

    Added async middleware

---
+++ higher-order-components/src/middlewares/async.js
@@ -0,0 +1,17 @@
+// export default function({ dispatch }) {
+//     // Next is a reference to the next middleware in the chain
+//     return function(next) {
+//         return function(action) {
+//
+//         }
+//     }
+// }
+
+// Above refactored to this ..
+export default ({ dispatch }) => next => action => {
+
+}
+
+
+
+
