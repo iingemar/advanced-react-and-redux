dbaad1bb1ccd4c53970d145b2290520f7d0df16a
Ingemar <iingemar@gmail.com>
2018-09-26 15:32:18 +0200

    Async: added check and dispatch

--- higher-order-components/src/middlewares/async.js
+++ higher-order-components/src/middlewares/async.js
@@ -9,7 +9,19 @@
 
 // Above refactored to this ..
 export default ({ dispatch }) => next => action => {
+    // Does the action.payload contain a Promise
+    // We assume that if payload have a then, it's a Promise.
+    if (!action.payload || !action.payload.then) {
+        // If it doesn't, send the action on to the next middleware in the chain.
+        return next(action);
+    }
 
+    // If it does, wait for it to finish
+    // ...action. Take all properties in action and add them
+    action.payload.then(function(response) {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+    });
 }
 
 
