1a06e9886a276d0b0070d1951cb23a8495ccb369
Ingemar <iingemar@gmail.com>
2018-10-04 12:08:54 +0200

    App setup: added morgan and bodyParser middleware

--- auth-server/server/index.js
+++ auth-server/server/index.js
@@ -6,6 +6,12 @@ const mongoose = require('mongoose');
 const app = express();
 
 // App setup
+// Register morgan as middleware. Morgan is a logging framework that logs all incoming requests.
+app.use(morgan('combined'));
+// Register bodyParser as middleware. Parses incoming requests.
+// Our setup parses all types as json, weird but that will do just now.
+app.use(bodyParser.json({ type: '*/*' }));
+
 
 // Server setup
 const port = process.env.PORT || 3090;
@@ -13,3 +19,6 @@ const server = http.createServer(app);
 server.listen(port);
 
 console.log('server listening on: ', port);
+
+
+
