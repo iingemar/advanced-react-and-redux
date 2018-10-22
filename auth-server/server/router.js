ace35579f0be39f705b29db17a3f1be58c5f1db4
Ingemar Finn <ingemar.finn@king.com>
2018-10-22 21:09:09 +0200

    Add authenticated route

--- auth-server/server/router.js
+++ auth-server/server/router.js
@@ -1,11 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false});
 
 // This is how we export in node.js
 module.exports = function(app) {
 
-    app.post('/signup', Authentication.signup);
-
-    app.get('/', function(req, res, next) {
-        res.send(['adsf', 'dsfasdf']);
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there'});
     });
+    
+    app.post('/signup', Authentication.signup);
 };
